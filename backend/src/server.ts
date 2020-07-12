058fd0f41043b7345534ee5447f5e53ac61a4ed1
RafaelCopes <rafaelcopes16@gmail.com>
2020-07-12 18:25:39 -0300

    starting application

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('The server is running on port 3333...'));
